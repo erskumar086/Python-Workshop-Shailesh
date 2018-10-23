a52b0976936997eea316272b7761f74bfb1bd462
shailesh kumar <shailesh.kumar19@wipro.com>
2018-10-23 11:27:14 +0530

    Update hands_on_exercise.py

---
+++ hands_on_exercise.py
@@ -0,0 +1,53 @@
+"""Intro to Python - Part 1 - Hands-On Exercise."""
+
+
+import math
+import random
+
+
+# TODO: Write a print statement that displays both the type and value of `pi`
+pi = math.pi
+print(type(pi), pi)
+
+
+# TODO: Write a conditional to print out if `i` is less than or greater than 50
+i = random.randint(0, 100)
+if(i<50):
+    print("i is less than 50=", i)
+elif(i>50):
+    print("i is greater than 50=", i)
+
+
+# TODO: Write a conditional that prints the color of the picked fruit
+picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
+if(picked_fruit== "orange"):
+    print("colour of fruit is orange", picked_fruit)
+elif(picked_fruit== "strawberry"):
+    print("colour of fruit is pink",picked_fruit)
+elif (picked_fruit== "banana"):
+    print("colour of fruit is yellow",picked_fruit)
+
+
+    
+# TODO: Write a function that multiplies two numbers and returns the result
+# Define the function here.
+a=int(input("enter the value of a="))
+b=int(input("enter the value of b="))
+def multiplies(a,b):
+    result = a*b
+    return result
+print("multiplication of a and b =",multiplies(a,b))
+
+
+
+
+
+
+
+# TODO: Now call the function a few times to calculate the following answers
+
+print("12 x 96 =",multiplies(12, 96))
+
+print("48 x 17 =",multiplies(48, 17))
+
+print("196523 x 87323 =",multiplies(196523, 87323))
